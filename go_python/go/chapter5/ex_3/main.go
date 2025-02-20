9641b514b613758598dc9b20f757e5ac742e537c
EpicBrainUser <max.tea@protonmail.com>
2025-02-20 22:15:00 +0100

    Add 5.3 in Go

--- go_python/go/chapter5/ex_3/main.go
+++ go_python/go/chapter5/ex_3/main.go
@@ -2,23 +2,33 @@ package main
 
 import "fmt"
 
-func getValidNumber(){
-  for {
+func getValidNumber() int {
     var number int
     fmt.Printf("Enter a valid number between 1 and 12\n")
     fmt.Scan(&number)
-    if number < 1 || number > 13 {
+    if number < 1 || number > 12 {
       fmt.Printf("Number not in range, please re-enter the number")
-      return number
-      break
-
+      getValidNumber()
+      return 0
     } else {
       fmt.Printf("Number in correct range, proceed. \n")
-      fmt.Scan(&number)
+      return number
+    }
+}
+
+func printTimesTable(number int) {
+  fmt.Printf("You entered: %v\nThe Times Tables for that number are:\n", number)
+  for i:=1; i<13; i++ {
+    result := number*i
+    if i < 10 {
+    fmt.Printf(" %v x %v = %v\n", i, number, result)
+    } else {
+    fmt.Printf("%v x %v = %v\n", i, number, result)
     }
   }
 }
 
 func main(){
-
+  number := getValidNumber()
+  printTimesTable(number)
 }
