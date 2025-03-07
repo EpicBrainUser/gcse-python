62d90ff1d824b148062e767c0bf6205c4fb4f3f1
Max Tea <max.tea@protonmail.com>
2025-03-07 18:23:10 +0000

    Solved and completed the 8.5 role play inventory management game:     This would be a useful exercise to look back upon, as I did the     following:         - Used nonlocal function         - imported files into each other         - error handling         - passing functions into other functions as parameters

--- python/8/roleplayGame/game.py
+++ python/8/roleplayGame/game.py
@@ -16,96 +16,13 @@
 #Please enter your choice: >>
 #Ideally you will use separate functions for each option, passing the updated inventory between the functions.
 
-#import simple_menu.py
-from simple_menu import simple_menu
+from simple_menu import menuCaller
+from game_func import game
 
-
-def game():
-    wanna_play = True
-    while wanna_play:
-        inventory = []
-    
-
-        def add_item_to_inventory():
-            item = input("Enter the item name to add: ")
-            inventory.append(item)
-            print(f"{item} has been added to your inventory.")
-
-#        def add_items_to_menu():
-#            print("This option isn't developed yet")
-        def remove_item_from_inventory():
-            print("This option isn't developed yet")
-    #   def get_item_from_inventory():
-    #   def view_selected_item():
-    #   def add_current_item_to_inventory():
-        
-        def quit():
-            print("sorry to see you go. ")
-            wanna_play = False
-    
-        def print_items():
-            print(f"Your inventory is: {inventory}")
-    
-        def actual_game_menu():
-            print("\t\tActual Game Menu\n")
-            print("\t\tA - Print Inventory Items\n\t\tB - Add Item To Inventory\n\t\tC - Remove Item From Inventory\n\t\tF - Quit")
-            
-            valid_option = ['A','B','C', 'D', 'E', 'F']
-            
-            while True:
-                selection = input("Please enter your choice: ").upper()
-                if selection in valid_option:
-                    print("That is a valid choice")
-                    return selection
-        #            break
-                else:
-                    print("That is not a valid choice")
-        def callEmAll():
-            while wanna_play:
-                playerSelection = actual_game_menu()
-
-            if playerSelection == 'A':
-                print_items()
-
-            elif playerSelection == 'B':
-                print("This option isn't developed yet")
-
-            elif playerSelection == 'C':
-                print("This option isn't developed yet")
-
-            elif playerSelection == 'D':
-                print("This option isn't developed yet")
-
-            elif playerSelection == 'E':
-                print("This option isn't developed yet")
-
-            elif playerSelection == 'F':
-                quit()
-
-        callEmAll()
-
-
-
-
-#simple_menu.simple_menu
-#simple_menu()
-
-#simple_menu.menuCaller
 
 def main():
-    while True:
-        userSelection = simple_menu()
-        if userSelection == 'A':
-            name = input("What is your name?")
-            print(f"Hello {name}, it's good to see you")
-        elif userSelection == 'B':
-            print("Game is starting...")
-            game()
-        elif userSelection == 'C':
-            print("Thank you for playing")
-            exit()
+    menuCaller(game)
 
-#selection = simple_menu()
-
-main()
+if __name__ == "__main__":
+    main()
 
